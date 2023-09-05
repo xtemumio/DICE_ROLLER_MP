4db011e2f3ad1e7406c39142afa08632778f10b0
paolo <mpaolo025@gmail.com>
2023-09-05 15:21:11 +0200

    commit con stampa dei valori in activity_main.xml programma terminato con aggiunta di commenti

--- app/src/main/java/com/example/dice_roller_pm/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller_pm/MainActivity.kt
@@ -9,32 +9,44 @@ import com.example.dice_roller_pm.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val diceList = mutableListOf<Int>()
-    private lateinit var adapter: ArrayAdapter<Int>
+    private val diceList = mutableListOf<Int>() // Lista per memorizzare i risultati dei dadi
+    private lateinit var adapter: ArrayAdapter<Int> // Adattatore per collegare la lista alla vista
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Collega il gestore di eventi al pulsante
         binding.button.setOnClickListener { rollDice() }
 
+        // Ottieni una riferimento alla ListView nel layout
         val listView = findViewById<ListView>(R.id.listView)
+
+        // Crea un adattatore per visualizzare i risultati della lista
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, diceList)
+
+        // Collega l'adattatore alla ListView
         listView.adapter = adapter
     }
 
     private fun rollDice() {
+        // Ottieni il numero di facce e il numero di dadi dalle caselle di testo nell'interfaccia utente
         val numFaces = binding.face.text.toString().toInt()
         val numDice = binding.dice.text.toString().toInt()
 
         diceList.clear() // Pulisci la lista prima di aggiungere nuovi risultati
 
+        // Crea un oggetto Dice con il numero di facce e di dadi specificato
         val dice = Dice(numFaces, numDice)
-        val result = dice.result()
-        diceList.addAll(result) // Aggiungi i nuovi risultati alla lista
 
-        // Notifica all'adapter che i dati sono cambiati
+        // Ottieni i risultati dei lanci di dadi
+        val result = dice.result()
+
+        // Aggiungi i nuovi risultati alla lista
+        diceList.addAll(result)
+
+        // Notifica all'adattatore che i dati sono cambiati, in modo che la ListView venga aggiornata
         adapter.notifyDataSetChanged()
     }
 }
@@ -42,16 +54,19 @@ class MainActivity : AppCompatActivity() {
 class Dice(private val numFaces: Int, private val numDice: Int) {
 
     private fun roll(): Int {
-        return (1..numFaces).random()
+        return (1..numFaces).random() // Simula il lancio di un dado e restituisce un numero casuale
     }
 
     fun result(): List<Int> {
         val result: MutableList<Int> = mutableListOf()
         var i = 0
+
+        // Esegui il lancio dei dadi il numero di volte specificato e aggiungi i risultati alla lista
         while (i < numDice) {
             result.add(roll())
             i++
         }
-        return result
+
+        return result // Restituisci la lista dei risultati dei lanci dei dadi
     }
 }
